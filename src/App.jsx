b2f9a29922a9f610f6b434f860d4a924ba0a3302
Rodrigo de Melo Rodrigues <rmelorodrigues@gmail.com>
2023-04-17 21:47:28 -0300

    Criando Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,15 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
+// import './App.css'
 import {InputText} from './components/inputText'
 import {MyTitle} from './components/myTitle'
+import { MyNavbar } from './components/MyNavbar'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-
-    <MyTitle title='Olá mundo, meu nome é Rodrigo!!!'/>
-    {/* <h1></h1> */}
-
-    <InputText 
-      id='nome'
-      name='nome'
-      placeholder='Nome'/>
-
-    {/* <input type="text" name="nome" id="nome" placeholder='Nome'/> */}
-    <br />
-
-    <InputText  
-      id='sobrenome'
-      name='sobrenome'
-      placeholder='Sobrenome'/>
-    {/* <input type="text" name="sobrenome" id="sobrenome" placeholder='Sobrenome'/> */}
-
+      <MyNavbar/>
     </>
 )}
 
